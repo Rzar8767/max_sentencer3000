dfe4c3b9931ebb3e881177e33fb3e4438407d621
Rzar8767 <33533895+Rzar8767@users.noreply.github.com>
2019-12-03 21:47:42 +0100

    basic sparql querying

--- lib/max_sentencer3000/dbpedia_query.ex
+++ lib/max_sentencer3000/dbpedia_query.ex
@@ -1,13 +1,34 @@
 defmodule MS3000.DbpediaQuery do
-  def test() do
-    query = """
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT DISTINCT ?resource ?p ?value where {
-    ?resource rdfs:label "screen"@en.
-    ?resource ?p ?value
-    }
-    """
-
+  @moduledoc """
+  Responsible for querying Dbpedia and handing over the result.
+  """
+  #TODO: GenServer with process pool/that spawns new processes
+  @spec query_dbpedia(String.t()) :: {:ok, %SPARQL.Query.Result{}} | {:error, Error.t()}
+  def query_dbpedia(entity_name) do
+    query = query_builder(entity_name)
     query |> SPARQL.Client.query("http://dbpedia.org/sparql")
+  end
+
+  defp query_builder(entity) do
+    _query2 = """
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX dbo: <http://dbpedia.org/ontology/>
+    select *
+    where {
+      {
+        ?person rdfs:label "#{entity}"@en.
+        ?person a dbo:Person
+      }
+      UNION {
+        ?place rdfs:label "#{entity}"@en.
+        ?place a dbo:Place
+      }
+      UNION {
+        ?organisation rdfs:label "#{entity}"@en.
+        ?organisation a dbo:Organisation
+      }
+    }
+    LIMIT 10
+    """
   end
 end
