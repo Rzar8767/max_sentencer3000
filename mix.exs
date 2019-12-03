93125a604ec8cc57925c9d463e910d4bff3b5d46
Rzar8767 <33533895+Rzar8767@users.noreply.github.com>
2019-12-03 11:06:13 +0100

    config, hackney dependency

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule MaxSentencer3000.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:sparql_client, "~> 0.2.2"}
+      {:sparql_client, "~> 0.2.2"},
+      {:hackney, "~> 1.15"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
